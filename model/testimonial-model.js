83a085d6ac0cfcaadc19bfce18d8857ee970d056
emondewan00 <dewanmohammademon@gmail.com>
2024-06-30 00:18:38 +0600

    add testimonial schema

---
+++ model/testimonial-model.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+
+const testimonialSchema = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  courseId: {
+    type: Schema.Types.ObjectId,
+    ref: "Course",
+    required: true,
+    index: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    index: true,
+  },
+});
+
+export const Testimonial =
+  mongoose.models.Testimonial ??
+  mongoose.model("Testimonial", testimonialSchema);
