86edf155b61bf4afc65dd09843c52701a7daa9b1
emondewan00 <dewanmohammademon@gmail.com>
2024-06-29 22:24:39 +0600

    add category schema

---
+++ model/category-model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const categorySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: false,
+  },
+});
+
+export const Category =
+  mongoose.models.Category ?? mongoose.model("Category", categorySchema);
