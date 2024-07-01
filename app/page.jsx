3b4bea33770a53e8c37626b169ca96a4afc6f07c
emondewan00 <dewanmohammademon@gmail.com>
2024-07-01 16:08:30 +0600

    home page design convert

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-import connectDB from "@/lib/connectDB";
-import { getCourses } from "@/queries/courses";
-
-export default async function Home() {
-  await connectDB();
-  const courses = await getCourses();
-  return <div>hello home </div>;
-}
