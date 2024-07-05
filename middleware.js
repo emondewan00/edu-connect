e010ddd4b449048c8faba16be5b00876ebd4dfbe
emondewan00 <dewanmohammademon@gmail.com>
2024-07-05 14:47:21 +0600

    protect privet route

--- middleware.js
+++ middleware.js
@@ -2,10 +2,23 @@ import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 const { auth } = NextAuth(authConfig);
 import { NextResponse } from "next/server";
+import { PUBLIC_ROUTES, LOGIN, ROOT } from "./lib/routes";
 export default auth((req) => {
-  //   if (!req.auth && req.nextUrl.pathname !== "/login") {
-  //     const newUrl = new URL("/login", req.nextUrl.origin);
-  //     return Response.redirect(newUrl);
-  //   }
+  const { nextUrl } = req;
+  const isAuthenticated = !!req.auth;
+  console.log(isAuthenticated, nextUrl?.pathname, "login in middleware");
+  const isPublicRoute = PUBLIC_ROUTES.find(
+    (r) => nextUrl.pathname.startsWith(r) || nextUrl.pathname === ROOT
+  );
+
+
+  if (!isAuthenticated && !isPublicRoute && nextUrl.pathname!== LOGIN) {
+    const newUrl = new URL(LOGIN, req.nextUrl.origin);
+    return NextResponse.redirect(newUrl);
+  }
   return NextResponse.next();
 });
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
