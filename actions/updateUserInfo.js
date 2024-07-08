59314898e25d7886237b23d6943cdf6af6a284c3
emondewan00 <dewanmohammademon@gmail.com>
2024-07-09 00:04:27 +0600

    add update user info function

---
+++ actions/updateUserInfo.js
@@ -0,0 +1,16 @@
+"use server";
+
+import connectDB from "@/lib/connectDB";
+import { User } from "@/model/user-model";
+import { revalidatePath } from "next/cache";
+
+export const updateUserInfo = async (email, data) => {
+  try {
+    await connectDB();
+    await User.findOneAndUpdate({ email }, data);
+    revalidatePath("/account");
+    return { message: "User info updated successfully", success: true };
+  } catch (error) {
+    return { message: "Error updating user info", success: false };
+  }
+};
