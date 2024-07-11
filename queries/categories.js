ea22332d5c7bcda08b601b20736cb24c8e64a042
emondewan00 <dewanmohammademon@gmail.com>
2024-07-11 23:58:57 +0600

    add get catogry by id function

--- queries/categories.js
+++ queries/categories.js
@@ -5,3 +5,8 @@ export const getCategories = async () => {
   const categories = await Category.find().lean();
   return replaceMongoIdInArray(categories);
 };
+
+export const getCategoryById = async (id) => {
+  const category = await Category.findById(id).lean();
+  return category;
+};
